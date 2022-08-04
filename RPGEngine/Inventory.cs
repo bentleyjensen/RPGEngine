5a374cbef7aa0bb6b047bfa8f92247d99ee68a5b
bentleyjensen <bentleyjensen@users.noreply.github.com>
2022-08-04 10:48:30 -0700

    Simplify OccpiedSize + small refactor

--- RPGEngine/Inventory.cs
+++ RPGEngine/Inventory.cs
@@ -26,40 +26,41 @@ namespace RPGEngine
         // Deafult for creating new characters, versus MaxSize (if the player buys a bigger pack)
         public static int DefaultMaxSize { get; set; } = 20;
         public int MaxSize { get; set; }
-        public int OccupiedSize { get
-            {
-                int calcOccupied = 0;
-                InventoryItems.ForEach(delegate(BaseItem item) {
-                    calcOccupied += item.Size;
-                });
-                return calcOccupied;
-            }
-        }
+        public int OccupiedSize { get; private set;}
 
         // Contains anything that extends BaseItem
         public List<BaseItem> InventoryItems { get; set; }
 
         // I'm under the impression that getting something from InventoryItems
         // will require some sort of validation or limiting in the future,
-        // so we're leaving this here for the time being
-        public List<BaseItem> getInventory()
+        // so we're leaving this here even though it feels redundant
+        public List<BaseItem> Get()
         {
             return InventoryItems;
         }
 
-        public bool addToInventory(BaseItem newItem) {
+        public bool Add(BaseItem newItem) {
             // If the item is too big, don't add it
             if (AllowOverflow == false && OccupiedSize + newItem.Size > MaxSize)
             {
                 return false;
             } else {
+                OccupiedSize += newItem.Size;
                 InventoryItems.Add(newItem);
                 return true;
             }
         }
 
-        public bool RemoveFromInventory(BaseItem oldItem) {
-            return InventoryItems.Remove(oldItem);
+        public bool Remove(BaseItem oldItem) {
+            bool removed = InventoryItems.Remove(oldItem);
+            
+            if (removed)
+            {
+                OccupiedSize -= oldItem.Size;
+                return true;
+            } else {
+                return false;
+            }
         }
 
 
