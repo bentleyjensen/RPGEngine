dac3a49d241b0a4559129d00c916c55d811d9563
bentleyjensen <bentleyjensen@users.noreply.github.com>
2022-08-12 13:53:35 -0700

    Update spelling, add summaries, WIP equip

--- RPGEngine/characters/Character.cs
+++ RPGEngine/characters/Character.cs
@@ -8,15 +8,15 @@ namespace RPGEngine
         public Character(string name, int startStat) : base(startStat)
         {
             Name = name;
-            Inventory = new Inventory(30, true);
-            EquipedItems = new List<EquipableItem> { };
+            Inventory = new Inventory(30, false);
+            EquippedItems = new List<EquipableItem> { };
             StatisticEffects = new List<Effect> { };
         }
 
         public string Name { get; set; }
         public int Money { get; set; }
         public Inventory Inventory { get; set; }
-        public List<EquipableItem> EquipedItems { get; set; }
+        public List<EquipableItem> EquippedItems { get; set; }
         public List<Effect> StatisticEffects { get; set; }
 
         public string[] EquipSlots =
@@ -39,13 +39,62 @@ namespace RPGEngine
             "Pet2",
         };
 
-        public void Equip() { 
+        /// <summary>
+        /// Equip an item, applying the effects to the character
+        /// </summary>
+        /// <param name="item">Item to equip</param>
+        /// <param name="equipSlot">Slot the item is equipped in</param>
+        /// <returns>True if the item was successfully equipped, false otherwise</returns>
+        public bool Equip(EquipableItem item, string equipSlot) {
+            // TODO: implement exceptions
+            // Check if item has the equipslot listed
+            if (false)
+                throw new Exception($"Item cannot be equiped in the {equipSlot} slot");
+            // Check equiped items to see if the slot is already taken
+            if (false)
+                throw new Exception($"There is already an item equiped in the {equipSlot} slot");
             // Remove from Inventory
+            if (false)
+                throw new Exception($"{item.Name} is not in the inventory");
+            Inventory.Remove(item);
             // Add to EquipedItems
+            EquippedItems.Add(item);
+            // Add status effects/enchantments to stats
+            foreach (Effect effect in item.EquipEffects)
+                StatisticEffects.Add(effect);
+            foreach (Effect effect in item.Enchantments)
+                StatisticEffects.Add(effect);
+            return true;
         }
-        public void Unequip() {
+
+        /// <summary>
+        /// Remove an actively equipped 
+        /// </summary>
+        /// <param name="item"></param>
+        public void Unequip(EquipableItem item) {
             // Remove from EquipedItems
             // Add to Inventory
+            // Remove status effects/enchantments to stats
+        }
+
+        /// <summary>
+        /// Apply the effects of ConsumableItem to oneself
+        /// </summary>
+        /// <param name="item">Item to consume</param>
+        public void Consume(ConsumableItem item) {
+            Console.WriteLine(item);
+            Console.WriteLine(item.Description);
+            return;
+        }
+
+        /// <summary>
+        /// Apply the effects of ConsumableItem to a target
+        /// </summary>
+        /// <param name="item">The item to throw</param>
+        /// <param name="target">A BaseCharacter to apply the effects to</param>
+        // What are we throwing, and who are we throwing it at?
+        public void Throw(ConsumableItem item, BaseCharacter target) {
+            return;
         }
         public void OnAttack() { }
         public void OnDefend() { }
