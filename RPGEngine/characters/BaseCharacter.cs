f5a652233f9d1f248c20ae4093b13d8254d148ef
bentleyjensen <bentleyjensen@users.noreply.github.com>
2022-08-12 13:50:35 -0700

    Add alignment

--- RPGEngine/characters/BaseCharacter.cs
+++ RPGEngine/characters/BaseCharacter.cs
@@ -9,6 +9,29 @@ namespace RPGEngine
         }
 
         public BaseStatistics statistics { get; set; }
+        public string Alignment {
+            get {
+                return Alignment;
+            }
+            set {
+                string[] Alignments =
+                {
+                    "LawfulGood",
+                    "NeutralGood",
+                    "ChaoticGood",
+                    "LawfulNeutral",
+                    "TrueNeutral",
+                    "ChaoticNeutral",
+                    "LawfulEvil",
+                    "NeutralEvil",
+                    "ChaoticEvil"
+                };
+                if (Alignments.Contains(value))
+                {
+                    Alignment = value;
+                }
+            }
+        }
 
     }
 }
