46e6b062b4d1cbdb5b80023fb157d93479cdd7a9
bentleyjensen <bentleyjensen@users.noreply.github.com>
2022-08-12 13:47:41 -0700

    Add summaries to Inventory

--- RPGEngine/Inventory.cs
+++ RPGEngine/Inventory.cs
@@ -9,6 +9,7 @@ namespace RPGEngine
         {
             MaxSize = DefaultMaxSize;
             InventoryItems = new List<BaseItem> {};
+            AllowOverflow = false;
         }
         public Inventory(int? size, bool allowOverflow)
         {
@@ -20,25 +21,45 @@ namespace RPGEngine
             AllowOverflow = allowOverflow;
             InventoryItems = new List<BaseItem> {};
         }
+
+        /// <summary>
+        /// The default inventory size when none is provided
+        /// </summary>
+        public static int DefaultMaxSize { get; set; } = 20;
+
+        /// <summary>
+        /// <c>MaxSize</c> is the size of the inventory that is enforced
+        /// </summary>
+        public int MaxSize { get; set; }
+
+        /// <summary>
+        /// The amount of inventory currently being used by the items it contains
+        /// </summary>
+        public int OccupiedSize { get; private set;}
+
+        /// <summary>
+        /// <c>AllowOverflow</c> determines whether items can be added if the total size would be greater than the MaxSize
+        /// </summary>
         // Not static, to support character false, and merchant true
         public bool AllowOverflow { get; set; }
 
-        // Deafult for creating new characters, versus MaxSize (if the player buys a bigger pack)
-        public static int DefaultMaxSize { get; set; } = 20;
-        public int MaxSize { get; set; }
-        public int OccupiedSize { get; private set;}
-
         // Contains anything that extends BaseItem
-        public List<BaseItem> InventoryItems { get; set; }
+        private List<BaseItem> InventoryItems { get; set; }
 
-        // I'm under the impression that getting something from InventoryItems
-        // will require some sort of validation or limiting in the future,
-        // so we're leaving this here even though it feels redundant
+        /// <summary>
+        /// Retrieve the full list of items in the inventory
+        /// </summary>
+        /// <returns>List&lt;BaseItem&gt; with current inventory items</returns>
         public List<BaseItem> Get()
         {
             return InventoryItems;
         }
 
+        /// <summary>
+        /// Add a single item to the inventory. Accepts any type that extends <c>BaseItem</c>.
+        /// </summary>
+        /// <param name="newItem">Item to add to the inventory</param>
+        /// <returns>False if the item could not be added, true otherwise</returns>
         public bool Add(BaseItem newItem) {
             // If the item is too big, don't add it
             if (AllowOverflow == false && OccupiedSize + newItem.Size > MaxSize)
@@ -51,6 +72,11 @@ namespace RPGEngine
             }
         }
 
+        /// <summary>
+        /// Remove a single item from the inventory. Accepts any type that extends <c>BaseItem</c>.
+        /// </summary>
+        /// <param name="oldItem">Item to remove from the inventory</param>
+        /// <returns>False if the item could not be removed, true otherwise</returns>
         public bool Remove(BaseItem oldItem) {
             bool removed = InventoryItems.Remove(oldItem);
             
